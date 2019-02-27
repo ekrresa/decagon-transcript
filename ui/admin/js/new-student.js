c7c62ae2a05647f895d85a8a53d27c6320dbe173
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-02-27 19:33:45 +0100

    populate student form with data from server

--- ui/admin/js/new-student.js
+++ ui/admin/js/new-student.js
@@ -1,7 +1,27 @@
+const urlParams = new URLSearchParams(window.location.search);
+const getUrl = `http://localhost:3000/students?email=${urlParams.get("email")}`;
+
+if (urlParams.has("email")) {
+  $.get(getUrl, function(data) {
+    let student = data[0];
+    $("#firstname").val(data[0].firstname);
+    $("#lastname").val(data[0].lastname);
+    $("#matric").val(data[0].matric_number);
+    $("#email").val(data[0].email);
+    $("#gender")
+      .val(data[0].gender)
+      .attr("selected", "selected");
+    $("#department").val(data[0].department);
+    $("#faculty").val(data[0].faculty);
+    $("#adm-year").val(data[0].year_of_admission);
+    $("#grad-year").val(data[0].graduation_year);
+    $("#cgpa").val(data[0].graduation_year);
+  });
+}
+
 $(document).ready(function() {
   let successDiv = $(".success");
   let errorDiv = $(".error");
-  console.log(successDiv);
 
   const student_form = $("form");
   student_form.submit(function(e) {
@@ -46,3 +66,5 @@ $(document).ready(function() {
       });
   });
 });
+
+function name(params) {}
