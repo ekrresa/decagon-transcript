851b92a0e6764ddd78d1bf501a6993834da45b63
Ben Mark <mquadrant@gmail.com>
2019-02-27 10:49:27 -0500

    Made the purpose of transcript dynamic in payment amount

--- ui/js/script.js
+++ ui/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     $("select.purpose").change(function(){
         selectedPurpose = $(this).children("option:selected").val();
         if(selectedPurpose == 'academic'){
+            nos=1;
             $('.apply-form table #academic_1').show();
             $('.apply-form table #personal').hide();
             $('div.hide-amount').text(nos);
@@ -14,20 +15,20 @@ $(document).ready(function(){
             $('.apply-form table #academic_2').hide();
             $('.apply-form table #academic_3').hide();
             $("#add-btn").removeAttr("disabled");
-            nos = 2;
+            nos = 1;
             $('div.hide-amount').text(nos);
         }
     });
 
     
     $("#add-btn").click(function(){
+        nos++;
         var tr = '<tr id="academic_'+nos+'"><td id="right"><label>Email '+nos+'</label></td><td id="left"><input type="email" name="personal-email-'+nos+'"+placeholder="abc@abc.com"></td></tr>';
         $(".apply-form table tbody").append(tr);
         $('div.hide-amount').text(nos);
         if(nos === 3){
             $("#add-btn").attr("disabled", "disabled");
         }
-        nos++;
     });
     
 
