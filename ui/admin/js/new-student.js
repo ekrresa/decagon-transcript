2fd36c119abefbd795c9cc317de111f686fa71ae
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-02-27 16:19:44 +0100

    accessed all data from form

---
+++ ui/admin/js/new-student.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  const student_form = $("form");
+  student_form.submit(function(e) {
+    e.preventDefault();
+    let firstname = $("#firstname").val();
+    let lastname = $("#lastname").val();
+    let matric = $("#matric").val();
+    let email = $("#email").val();
+    let gender = $("#gender").val();
+    let department = $("#department").val();
+    let faculty = $("#faculty").val();
+    let adm_year = $("#adm-year").val();
+    let grad_year = $("#grad-year").val();
+    let cgpa = $("#cgpa").val();
+
+    let formdata = {
+      firstname,
+      lastname,
+      matric,
+      email,
+      gender,
+      department,
+      faculty,
+      adm_year,
+      grad_year,
+      cgpa
+    };
+    console.log(formdata);
+  });
+});
