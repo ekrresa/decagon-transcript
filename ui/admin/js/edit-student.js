b2ec2c40eecdc5762c8d40391a54bf02573a48d7
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-02-28 01:22:41 +0100

    wrote script to update student details

--- ui/admin/js/edit-student.js
+++ ui/admin/js/edit-student.js
@@ -1,4 +1,6 @@
 const getUrl = `http://localhost:3000/students?email=${urlParams.get("email")}`;
+const student_form = $("form");
+let createDate;
 // Get student data, populate form fields
 $.get(getUrl, function(data) {
   let student = data[0];
@@ -14,4 +16,53 @@ $.get(getUrl, function(data) {
   $("#adm-year").val(data[0].year_of_admission);
   $("#grad-year").val(data[0].graduation_year);
   $("#cgpa").val(data[0].graduation_year);
+  student_form.attr("action", `http://localhost:3000/students/${data[0].id}`);
+  createDate = data[0].createdAt;
+});
+
+$(document).ready(function() {
+  let successDiv = $(".success");
+  let errorDiv = $(".error");
+
+  student_form.submit(function(e) {
+    e.preventDefault();
+    let firstname = $("#firstname").val();
+    let lastname = $("#lastname").val();
+    let matric = $("#matric").val();
+    let email = $("#email").val();
+    let gender = $("#gender").val();
+    let department = $("#department").val();
+    let faculty = $("#faculty").val();
+    let adm_year = $("#adm-year").val();
+    let grad_year = $("#grad-year").val();
+    let cgpa = $("#cgpa").val();
+    const url = $(this).attr("action");
+
+    let formdata = {
+      firstname,
+      lastname,
+      matric,
+      email,
+      gender,
+      department,
+      faculty,
+      adm_year,
+      grad_year,
+      cgpa,
+      createdAt: createDate,
+      updatedAt: new Date()
+    };
+
+    $.ajax({
+      type: "PUT",
+      url,
+      data: formdata
+    })
+      .done(res => {
+        successDiv.fadeIn().text("Student Updated Successfully");
+      })
+      .fail(err => {
+        errorDiv.fadeIn().text("There was an error. Please try again");
+      });
+  });
 });
