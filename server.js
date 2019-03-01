6f05ab5e663d03819ee16d602a56895c43dfd695
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-03-01 09:57:11 +0100

    configured app to push to heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middleware = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+server.use(middleware);
 server.use(router);
 
 server.listen(port);
