44103142a8594fa619c2550c7a6c3dee8466fd5e
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-03-09 18:59:58 +0100

    added validation to login form

--- public/ui/admin/js/login.js
+++ public/ui/admin/js/login.js
@@ -1,28 +1,39 @@
 $(document).ready(function() {
-  let errorDiv = $(".error");
   $("#loginForm").submit(function(e) {
     e.preventDefault();
     const email = $("#email").val();
     const password = $("#password").val();
-    const url = $(this).attr("action");
+    if (!email || !password) {
+      swal({
+        title: "Error!",
+        text: "All fields are required!",
+        icon: "error",
+        button: "Close"
+      });
+      return;
+    }
 
-    const getUrl = url + `?email=${email}`;
+    const getUrl = $(this).attr("action") + `?email=${email}`;
 
     $.get(getUrl, function(data) {
       if (data.length === 0) {
-        errorDiv
-          .text("Invalid email/password details. Please try again")
-          .fadeIn()
-          .fadeOut(5000);
+        swal({
+          title: "Error!",
+          text: "Invalid email/password details. Please try again",
+          icon: "error",
+          button: "Close"
+        });
       } else if (data[0].email === email && data[0].password === password) {
         localStorage.setItem("admin_email", email);
         localStorage.setItem("admin_name", data[0].name);
         window.location.replace("../../ui/admin/dashboard.html");
       } else {
-        errorDiv
-          .fadeIn()
-          .text("Invalid email/password details. Please try again")
-          .fadeOut(5000);
+        swal({
+          title: "Error!",
+          text: "Invalid email/password details. Please try again",
+          icon: "error",
+          button: "Close"
+        });
       }
     });
   });
