68c5f5d477eb57c8d3e7936d46a7a800173ff6b4
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-03-09 15:17:27 +0100

    added form validation with jquery

--- public/ui/admin/js/new-student.js
+++ public/ui/admin/js/new-student.js
@@ -1,56 +1,84 @@
 $(document).ready(function() {
   const student_form = $("form");
+
   student_form.submit(function(e) {
     e.preventDefault();
+    let valid = $(this).validate({
+      rules: {
+        firstname: "required",
+        lastname: "required",
+        matric: "required",
+        email: {
+          required: true,
+          email: true
+        },
+        gender: "required",
+        department: "required",
+        faculty: "required",
+        adm_year: {
+          required: true,
+          number: true
+        },
+        grad_year: {
+          required: true,
+          number: true
+        },
+        cgpa: "required"
+      },
+      messages: {
+        name: "Please specify your name",
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        adm_year: {
+          number: "Please enter a valid year"
+        },
+        grad_year: {
+          number: "Please enter a valid year"
+        }
+      }
+    });
+    if (valid.form()) {
+      const url = $(this).attr("action");
 
-    let firstname = $("#firstname").val();
-    let lastname = $("#lastname").val();
-    let matric = $("#matric").val();
-    let email = $("#email").val();
-    let gender = $("#gender").val();
-    let department = $("#department").val();
-    let faculty = $("#faculty").val();
-    let adm_year = $("#adm-year").val();
-    let grad_year = $("#grad-year").val();
-    let cgpa = $("#cgpa").val();
-    const url = $(this).attr("action");
+      let formData = {
+        firstname: $("#firstname").val(),
+        lastname: $("#lastname").val(),
+        matric: $("#matric").val(),
+        email: $("#email").val(),
+        gender: $("#gender").val(),
+        department: $("#department").val(),
+        faculty: $("#faculty").val(),
+        adm_year: $("#adm-year").val(),
+        grad_year: $("#grad-year").val(),
+        cgpa: $("#cgpa").val(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
 
-    let formdata = {
-      firstname,
-      lastname,
-      matric,
-      email,
-      gender,
-      department,
-      faculty,
-      adm_year,
-      grad_year,
-      cgpa,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    };
-
-    $.ajax({
-      type: "POST",
-      url,
-      data: formdata
-    })
-      .done(res => {
-        $("form")[0].reset();
-        swal({
-          title: "Good job!",
-          text: "Student created successfully",
-          icon: "success",
-          button: "Close"
-        });
+      $.ajax({
+        type: "POST",
+        url,
+        data: formData
       })
-      .fail(err => {
-        eswal({
-          title: "Good job!",
-          text: "An error occurred. Please try again!",
-          icon: "error",
-          button: "Close"
+        .done(res => {
+          $("form")[0].reset();
+          swal({
+            title: "Good job!",
+            text: "Student created successfully",
+            icon: "success",
+            button: "Close"
+          });
+        })
+        .fail(err => {
+          swal({
+            title: "Oops!",
+            text: "An error occurred. Please try again!",
+            icon: "error",
+            button: "Close"
+          });
         });
-      });
+    }
   });
 });
