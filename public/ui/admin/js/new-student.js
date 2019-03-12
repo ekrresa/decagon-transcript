3098e8ba034990d14008c60fc61c820f55426ab3
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-03-12 11:15:56 +0100

    validate student email/matric number on student creation

--- public/ui/admin/js/new-student.js
+++ public/ui/admin/js/new-student.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 
   student_form.submit(function(e) {
     e.preventDefault();
+    // Form validation
     let valid = $(this).validate({
       rules: {
         firstname: "required",
@@ -39,56 +40,95 @@ $(document).ready(function() {
         }
       }
     });
+
     if (valid.form()) {
-      const url = $(this).attr("action");
+      const student_email = $("#email").val();
+      const matric_number = $("#matric").val();
 
-      let formData = {
-        firstname: $("#firstname")
-          .val()
-          .toLowerCase(),
-        lastname: $("#lastname")
-          .val()
-          .toLowerCase(),
-        matric: $("#matric").val(),
-        email: $("#email").val(),
-        gender: $("#gender")
-          .val()
-          .toLowerCase(),
-        department: $("#department")
-          .val()
-          .toLowerCase(),
-        faculty: $("#faculty")
-          .val()
-          .toLowerCase(),
-        adm_year: $("#adm-year").val(),
-        grad_year: $("#grad-year").val(),
-        cgpa: $("#cgpa").val(),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      };
+      $.when(
+        $.get(`http://localhost:3000/students?email=${student_email}`),
+        $.get(`http://localhost:3000/students?matric=${matric_number}`)
+      ).then(function(res1, res2) {
+        if (res1[0].length === 0 && res2[0].length === 0) {
+          const url = "http://localhost:3000/students";
 
-      $.ajax({
-        type: "POST",
-        url,
-        data: formData
-      })
-        .done(res => {
-          $("form")[0].reset();
+          // Get form values
+          let formData = {
+            firstname: $("#firstname")
+              .val()
+              .toLowerCase(),
+            lastname: $("#lastname")
+              .val()
+              .toLowerCase(),
+            matric: matric_number,
+            email: student_email,
+            gender: $("#gender")
+              .val()
+              .toLowerCase(),
+            department: $("#department")
+              .val()
+              .toLowerCase(),
+            faculty: $("#faculty")
+              .val()
+              .toLowerCase(),
+            adm_year: $("#adm-year").val(),
+            grad_year: $("#grad-year").val(),
+            cgpa: $("#cgpa").val(),
+            createdAt: new Date(),
+            updatedAt: new Date()
+          };
+
+          // Create student
+          $.ajax({
+            type: "POST",
+            url,
+            data: formData
+          })
+            .done(res => {
+              $("form")[0].reset();
+              const password = generatePassword();
+              const studentId = res.id;
+
+              const newPassword = {
+                password,
+                studentId
+              };
+
+              $.post("http://localhost:3000/passwords", newPassword, function(res) {
+                swal({
+                  title: "Good job!",
+                  text: "Student created successfully",
+                  icon: "success",
+                  button: "Close"
+                });
+              });
+            })
+            .fail(err => {
+              swal({
+                title: "Oops!",
+                text: "An error occurred. Please try again!",
+                icon: "error",
+                button: "Close"
+              });
+            });
+        } else {
           swal({
-            title: "Good job!",
-            text: "Student created successfully",
-            icon: "success",
-            button: "Close"
-          });
-        })
-        .fail(err => {
-          swal({
-            title: "Oops!",
-            text: "An error occurred. Please try again!",
+            title: "Error!",
+            text: "Student with this email/matric number has been registered!",
             icon: "error",
             button: "Close"
           });
-        });
+          return;
+        }
+      });
+
+      return;
     }
   });
 });
+
+function generatePassword() {
+  return Math.random()
+    .toString(36)
+    .slice(-8);
+}
